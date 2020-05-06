ce46abcae7b472c33e787996a73e77fb4b7a6050
adithya2000 <adithya.narayan2000@gmail.com>
2020-05-06 16:44:07 +0530

    Js file

--- login.js
+++ login.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-
+//var multer=require('multer');
+//var fs = require('fs');
+// var storage = multer.diskStorage({
+// 	destination:function(req,file,cb){
+// 		cb(null,'./uploads/');
+// 	},
+// 	filename:function(req,file,cb){
+// 		cb(null,Date.now() + file.originalname);
+// 	}
+// });
+//var upload = multer({storage:storage});
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -26,6 +36,19 @@ conn.connect(function (err){
 
 })
 
+// app.get('/image/:username',function(req,resp){
+// 	conn.query('SELECT img from User WHERE username =?',[req.params.username],
+// 		function(error,rows,fields){
+// 			if(!!error){
+// 				console.log('Error');
+// 			}
+// 			else{
+// 				resp.json(rows);
+// 			}
+// 	}),
+
+// })
+
 
 app.get('/:username',function(req,resp){
 	conn.query('SELECT * FROM User WHERE username = ?',[req.params.username],
@@ -49,7 +72,57 @@ app.post('/register',function(req,res){
     }
   });
 })
+app.get('/',function(req,res){
+	conn.query('SELECT group_id FROM group_reg',function(error,rows,fields){
+		if(!!error)
+		console.log('Error');
+		else {
+			res.send(JSON.stringify(rows));
+			console.log('hi');
+		}
+	});
+})
+app.post('/atg/:username',function(req,res){
+  var postData=req.body;
+  conn.query('UPDATE User SET group_id=? WHERE username=?',[req.body.group_id,req.body.username],postData,function(error,rows,fields){
+    if(!!error){
+      console.log(error);
+    }
+    else{
+      res.send(JSON.stringify(rows));
+    }
+  });
+})
+app.post('/grpreg',function(req,res){
+  var postData=req.body;
+  conn.query('INSERT INTO group_reg SET ?',postData,function(error,rows,fields){
+    if(!!error){
+      console.log(error);
+    }
+    else{
+      res.send(JSON.stringify(rows));
+    }
+  });
+})
 
+// var image = {
+// 	img: fs.readFileSync("/opt/lampp/htdocs/events/uploads/liverpool.jpg",{encoding:'utf8', flag:'r'},
+// 	file_name ='Livpool'
+// )};
+
+
+// app.post('/upload',upload.single('picture'),function(req,res){
+// 	if(!req.file){
+// 		console.log('No FIle Received');
+// 	}
+// 	else{
+// 		console.log(req.file);
+// 		console.log('File Received');
+// 		conn.query('INSERT INTO User SET ?',req,function(err,res){
+// 	 	console.log(res);
+// 	 });
+// 	}
+// });
 
 app.post('/loan/:username',function(req,resp){
 	//console.log(req.body);
